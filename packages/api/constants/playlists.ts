033fbb5c8bc43c37fdd514eba4eb9b0932602b59
Thibaut <thibaut.pernin@gmail.com>
2024-08-22 17:09:08 +0200

    feat(api): leaderboard by country

---
+++ packages/api/constants/playlists.ts
@@ -0,0 +1,12 @@
+export type Country = 'WW' | 'BR' | 'DE' | 'ES' | 'FR' | 'IT' | 'PT' | 'US';
+
+export const playlistIds: { [key in Country]: string } = {
+	WW: '37i9dQZEVXbMDoHDwVN2tF', // Worldwide
+	BR: '37i9dQZEVXbMXbN3EUUhlg',
+	DE: '37i9dQZEVXbJiZcmkrIHGU',
+	ES: '37i9dQZEVXbNFJfN1Vw8d9',
+	FR: '37i9dQZEVXbIPWwFssbupI',
+	IT: '37i9dQZEVXbIQnj7RRhdSX',
+	PT: '37i9dQZEVXbKyJS56d1pgi',
+	US: '37i9dQZEVXbLRQDuF5jeBp',
+};
--- packages/api/index.ts
+++ packages/api/index.ts
@@ -2,21 +2,34 @@ import express, { Request, Response } from 'express';
 import bodyParser from 'body-parser';
 import axios from 'axios';
 import dotenv from 'dotenv';
+import { Country, playlistIds } from './constants/playlists';
 
 dotenv.config();
 
-const {
-	SPOTIFY_CLIENT_ID,
-	SPOTIFY_CLIENT_SECRET,
-	SPOTIFY_TOP_50_WORLD_PLAYLIST_ID,
-} = process.env;
+const { SPOTIFY_CLIENT_ID, SPOTIFY_CLIENT_SECRET } = process.env;
 
 const app = express();
 app.use(bodyParser.json());
 
-let leaderboard: any[] = [];
+interface LeaderboardEntry {
+	rank: number;
+	artist: {
+		name: string;
+		image: string;
+	};
+	track: {
+		name: string;
+		url: string;
+	};
+}
 
-const getSpotifyToken = async () => {
+type Leaderboards = {
+	[key in Country]?: LeaderboardEntry[];
+};
+
+let leaderboard: Leaderboards = {};
+
+const getSpotifyToken = async (): Promise<string> => {
 	const tokenResponse = await axios.post(
 		'https://accounts.spotify.com/api/token',
 		'grant_type=client_credentials',
@@ -33,12 +46,17 @@ const getSpotifyToken = async () => {
 	return tokenResponse.data.access_token;
 };
 
-const fetchLeaderboard = async () => {
+const fetchLeaderboard = async (country: Country) => {
 	try {
 		const accessToken = await getSpotifyToken();
+		const playlistId = playlistIds[country];
+
+		if (!playlistId) {
+			throw new Error(`No playlist found for country: ${country}`);
+		}
 
 		const response = await axios.get(
-			`https://api.spotify.com/v1/playlists/${SPOTIFY_TOP_50_WORLD_PLAYLIST_ID}/tracks?limit=50`,
+			`https://api.spotify.com/v1/playlists/${playlistId}/tracks?limit=50`,
 			{
 				headers: {
 					Authorization: `Bearer ${accessToken}`,
@@ -46,35 +64,82 @@ const fetchLeaderboard = async () => {
 			}
 		);
 
-		leaderboard = response.data.items.map((item: any, index: number) => ({
-			rank: index + 1,
-			artist: item.track.artists[0].name,
-			track: item.track.name,
-		}));
+		// console.log(response.data.items);
 
-		console.log('Leaderboard updated:', leaderboard);
+		leaderboard[country] = response.data.items.map(
+			(item: any, index: number) => ({
+				rank: index + 1,
+				artist: {
+					name: item.track.artists[0].name,
+				},
+				track: {
+					name: item.track.name,
+					url: item.track.href,
+				},
+			})
+		);
+
+		console.log(
+			`Leaderboard updated for ${country}:`,
+			leaderboard[country]
+		);
 	} catch (error) {
-		console.error('Error fetching leaderboard:', error);
+		console.error(`Error fetching leaderboard for ${country}:`, error);
 	}
 };
 
-app.post('/update-leaderboard', async (req: Request, res: Response) => {
-	await fetchLeaderboard();
-	res.json({ message: 'Leaderboard updated' });
-});
+app.post(
+	'/update-leaderboard/:country',
+	async (req: Request, res: Response) => {
+		const country = req.params.country.toUpperCase() as Country;
+		if (!playlistIds[country]) {
+			return res
+				.status(400)
+				.json({ error: `Invalid country code: ${country}` });
+		}
+		await fetchLeaderboard(country);
+		res.json({ message: `Leaderboard updated for ${country}` });
+	}
+);
 
-app.get('/daily-winner', (req: Request, res: Response) => {
-	if (leaderboard.length === 0) {
-		return res.status(404).json({ error: 'Leaderboard not available' });
+app.get('/daily-winner/:country', (req: Request, res: Response) => {
+	const country = req.params.country.toUpperCase() as Country;
+
+	if (!leaderboard[country]) {
+		return res
+			.status(404)
+			.json({ error: `Leaderboard not available for ${country}` });
 	}
 
-	const winner = leaderboard[0];
-	res.json(winner.artist);
+	const winner = leaderboard[country]?.[0].artist.name;
+	res.json(winner);
 });
 
-setInterval(fetchLeaderboard, 24 * 60 * 60 * 1000);
+app.get('/leaderboard/:country', (req: Request, res: Response) => {
+	const country = req.params.country.toUpperCase() as Country;
+
+	if (!leaderboard[country]) {
+		return res
+			.status(404)
+			.json({ error: `Leaderboard not available for ${country}` });
+	}
+
+	res.json({
+		message: `Current leaderboard for ${country}`,
+		leaderboard: leaderboard[country],
+	});
+});
+
+setInterval(() => {
+	Object.keys(playlistIds).forEach((country) =>
+		fetchLeaderboard(country as Country)
+	);
+}, 24 * 60 * 60 * 1000); // Fetch daily
 
 app.listen(8080, () => {
-	console.log('External adapter listening on port 8080');
-	fetchLeaderboard();
+	console.log('Server is up and listening on port 8080');
+
+	Object.keys(playlistIds).forEach((country) =>
+		fetchLeaderboard(country as Country)
+	);
 });
